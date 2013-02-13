eb983fba3600b0a050d175397eb8d9da2714e6f3
Name test <testmail@mailtest.cc>
2013-02-13 13:09:59 -0500

    Update numm for write console erros and fix emailAddress

--- NUMM/views/solicitStatus/map.js
+++ NUMM/views/solicitStatus/map.js
@@ -1,6 +1,6 @@
 function (doc){
 	if("numm.signup" in doc)
-		emit(doc["numm.signup"].emailAddress, 0x2);
+		emit(doc["numm.signup"].email, 0x2);
 	else{
 		var parts = doc._id.split(".");
 		if(parts[0] == "numm" && parts[1] == "solicit")
